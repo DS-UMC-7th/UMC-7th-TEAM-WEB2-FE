c21230e85738000e53d5900a4d96b7434c4cd9a5
jinhyo <kjhyo0220@gmail.com>
2024-11-25 05:34:03 +0900

    [Feat] 검색 기능 구현

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,31 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
 import logo from "../../assets/img/logo.png";
 import { ImgDiv, Img } from "../Footer/Footer.style";
 import * as S from "./Navbar.style";
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+
+  const [searchParams, setSearchParams] = useSearchParams({ query: "" });
+  const query = searchParams.get("query");
+
+  const onChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const onKeyDown = (e) => {
+    if (e.key === "Enter") {
+      onClick();
+    }
+  };
+
+  const onClick = () => {
+    if (query === searchValue) return;
+    navigate(`/list?query=${encodeURIComponent(searchValue)}`);
+  };
+
   return (
     <nav>
       <S.NavContainer>
@@ -21,8 +43,13 @@ const Navbar = () => {
         </S.NavDiv>
 
         <S.SearchDiv>
-          <S.SearchInput placeholder="강의명을 검색하면 연관된 리뷰를 보여드릴게요."></S.SearchInput>
-          <S.SearchButton></S.SearchButton>
+          <S.SearchInput
+            placeholder="강의명을 검색하면 연관된 리뷰를 보여드릴게요."
+            value={searchValue}
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+          ></S.SearchInput>
+          <S.SearchButton onClick={onClick} />
         </S.SearchDiv>
       </S.NavContainer>
     </nav>
