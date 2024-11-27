de2373f012944d933d15b55e40ee8fc746892a2a
jinhyo0 <150879545+jinhyo0@users.noreply.github.com>
2024-11-27 11:45:35 +0900

    Revert "Feat/#15/detail page"

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,26 @@ import Homepage from "./pages/HomePage/HomePage.jsx";
 import DetailPage from "./pages/DetailPage/DetailPage.jsx";
 import ListPage from "./pages/ListPage/ListPage.jsx";
 import PostPage from "./pages/PostPage/PostPage.jsx";
-import GlobalStyle from "./styles/GlobalStyle.js";
-import { ThemeProvider } from "styled-components";
-import theme from "./styles/theme.js";
-import Footer from "./components/Footer/Footer.jsx";
-import "./App.css";
-import RootLayout from "./components/layout/RootLayout.jsx";
+import GlobalStyle from './styles/GlobalStyle.js'; 
+import { ThemeProvider } from 'styled-components'; 
+import theme from './styles/theme.js'; 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <RootLayout />,
-    children: [
-      {
-        index: true,
-        element: <Homepage />,
-      },
-      { path: "list", element: <ListPage /> },
-      { path: "post", element: <PostPage /> },
-      { path: "detail", element: <DetailPage /> },
-    ],
+    element: <Homepage />,
+  },
+  {
+    path: "/list",
+    element: <ListPage />,
+  },
+  {
+    path: "/post",
+    element: <PostPage />,
+  },
+  {
+    path: "/detail",
+    element: <DetailPage />,
   },
 ]);
 
@@ -30,9 +30,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div className="App">
-        <RouterProvider router={router} />
-      </div>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
