36ecd1d60bd9dc703a9c6bfe0105d143b9c0c602
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2024-12-03 02:58:08 +0900

    chore: 불필요한 console 삭제

--- src/apis/home/getHomeReviews.js
+++ src/apis/home/getHomeReviews.js
@@ -3,8 +3,6 @@ import instance from "./instance";
 export const getRecommendedReviews = async () => {
   try {
     const response = await instance.get("/api/reviews?sort=recommended");
-    console.log("추천", response.data);
-
     return response.data.slice(0, 4);
   } catch (error) {
     console.error("추천 리뷰 가져오기 실패:", error);
@@ -15,8 +13,6 @@ export const getRecommendedReviews = async () => {
 export const getLatestReviews = async () => {
   try {
     const response = await instance.get("/api/reviews?sort=latest");
-    console.log(response.data);
-
     return response.data.slice(0, 4);
   } catch (error) {
     console.error("최신 리뷰 가져오기 실패:", error);
